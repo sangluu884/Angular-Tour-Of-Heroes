35d54ff53833db0c468b6f528b907d193f588348
sangluu884 <sangluu884@gmail.com>
2017-05-12 19:47:33 +0700

    Tour heroes
    20170512

---
+++ src/app/app.module.ts
@@ -0,0 +1,44 @@
+import { BrowserModule } 	from '@angular/platform-browser';
+import { NgModule } 		from '@angular/core';
+import { FormsModule } 		from '@angular/forms';
+import { HttpModule } 		from '@angular/http';
+
+//import { RouterModule }   from '@angular/router';
+import { AppRoutingModule } from './app-routing.module';
+
+//Imports for loading & configuring the in-memory web api
+import { InMemoryWebApiModule } from 'angular-in-memory-web-api';
+import { InMemoryDataService }  from './in-memory-data.service';
+
+import { AppComponent } 		from './app.component';
+import { HeroDetailComponent } 	from './hero-detail.component';
+import { HeroesComponent }     	from './heroes.component';
+import { HeroService }         	from './hero.service';
+import { DashboardComponent }   from './dashboard.component';
+
+
+
+
+@NgModule({
+  declarations: [
+    AppComponent,
+	HeroDetailComponent,
+	HeroesComponent,
+	DashboardComponent
+  ],
+  imports: [
+    BrowserModule,
+    FormsModule,
+	InMemoryWebApiModule.forRoot(InMemoryDataService),
+	AppRoutingModule,
+    HttpModule
+  ],
+  providers: [
+	HeroService
+  ],
+  bootstrap: [
+	AppComponent
+	//Hero
+  ]
+})
+export class AppModule { }
